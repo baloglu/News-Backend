5d61b66dd3063ca257ef459732e60caf65ea6c27
Chetin Baloglu <baloglu@gmail.com>
2023-05-09 14:33:21 +0100

    updating basic api/topics test

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -12,13 +12,15 @@ afterAll(() => {
     return db.end()
 })
 describe("API endpoints", () => {
-    test("/api/topics endpoint should fetch all topics", () => {
+    test("/api/topics endpoint should fetch an array of 3 objects with slug and description properties", () => {
         return request(app)
         .get("/api/topics")
         .expect(200)
-            .expect((result) => {
-            const expected = [{"description": "The man, the Mitch, the legend", "slug": "mitch"}, {"description": "Not dogs", "slug": "cats"}, {"description": "what books are made of", "slug": "paper"}]
-          expect(result.body).toEqual(expected);
+            .then((result) => {
+                const topics = result.body;
+                expect(topics).toHaveLength(3)
+                expect(topics[0]).toHaveProperty('description')
+                expect(topics[0]).toHaveProperty('slug')
         });
     })
 })
--- models/get-topics.model.js
+++ models/get-topics.model.js
@@ -1,7 +1,7 @@
 const db = require('../db/connection');
 
 function getTopicsModel() {
-    return db.query('SELECT * FROM topics;');
+    return db.query('SELECT slug, description FROM topics;');
 }
 
 module.exports = getTopicsModel
